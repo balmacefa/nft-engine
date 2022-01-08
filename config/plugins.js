cb53c0f35888f3a957e5440ae404a4d71f603ab2
Fabian Balmaceda Rescia <fabianbalmaceda@ibm.com>
2022-01-08 09:52:46 -0600

    nft engine init

---
+++ config/plugins.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'NFT_Engine': {
+        enabled: true,
+        resolve: './src/plugins/NFT_Engine'
+    },
+}
---
+++ src/plugins/NFT_Engine/README.md
@@ -0,0 +1,3 @@
+# Strapi plugin NFT_Engine
+
+A quick description of NFT_Engine.
